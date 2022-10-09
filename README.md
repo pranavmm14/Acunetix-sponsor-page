a9cbeb359fdc74b42eac7718f9a18a8985c72620
Pranav Mehendale <105978137+pranavmm14@users.noreply.github.com>
2022-10-09 23:04:13 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+To see working of the website you need to follow following steps:
+1) You need to have python flask module in your PC.
+2) Got to project folder and run web.py file
+3) In your browser go to http://localhost:5000 or to the port on which the web server is running(it can be found on the terminal)
+
---
+++ static/jscript.js
@@ -0,0 +1,6 @@
+document.getElementById("S1").addEventListener("mouseover", function () {
+   document.getElementsById("S1").style.backgroundColor="#ffffff" 
+});
+document.getElementsByClassName("spon_text").addEventListener("mouseover", function () {
+   document.getElementsByClassName("spon_text").style.backgroundColor="Red"
+});
